be45d74f1c68e9435375351f0c7f302ab2e54d44
Darshilp326 <darshil_1901me18@iitp.ac.in>
2020-09-20 00:22:42 +0530

    day2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+const name = "Darshil";
+const JsElement = <h1>I am {name}</h1>;
+const jsxElement = <div>I Love JSX!</div>;
+const jsxElementWithChild = (
+  <div>
+    <h1>I am a Header.</h1>
+    <h1>I am a Header too.</h1>
+  </div>
+);
+function giveNumber(bakery) {
+  return bakery.cakes + bakery.icecream;
+}
+const bakery = {
+  cakes: 10,
+  icecream: 20,
+};
+const jsElement = <h1>{giveNumber(bakery)}</h1>;
 
+const JsxWithClass = <div className="Hi">I am div tag with classname hi</div>;
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  [JsElement, jsxElement, jsxElementWithChild, JsxWithClass, jsElement],
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
