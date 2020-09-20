96c7ca5377d76c792f8c53b8d7ddf77403116d2a
Darshilp326 <darshil_1901me18@iitp.ac.in>
2020-09-21 00:21:56 +0530

    day3

--- src/index.js
+++ src/index.js
@@ -3,29 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-const name = "Darshil";
-const JsElement = <h1>I am {name}</h1>;
-const jsxElement = <div>I Love JSX!</div>;
-const jsxElementWithChild = (
-  <div>
-    <h1>I am a Header.</h1>
-    <h1>I am a Header too.</h1>
-  </div>
-);
-function giveNumber(bakery) {
-  return bakery.cakes + bakery.icecream;
+function clock() {
+  const element = (
+    <div>
+      <h2>Time now is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
 }
-const bakery = {
-  cakes: 10,
-  icecream: 20,
-};
-const jsElement = <h1>{giveNumber(bakery)}</h1>;
 
-const JsxWithClass = <div className="Hi">I am div tag with classname hi</div>;
-ReactDOM.render(
-  [JsElement, jsxElement, jsxElementWithChild, JsxWithClass, jsElement],
-  document.getElementById("root")
-);
+setInterval(clock, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
