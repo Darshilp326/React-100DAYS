caa3248cdcc1a11b0ae567a42901b55a5d0e6867
Darshilp326 <darshil_1901me18@iitp.ac.in>
2020-09-21 23:53:14 +0530

    Day4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-const element = <h1>Hello, world!</h1>;
-function App() {
-  return <div className="App">element</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "Click here!" };
+  }
+  changeState = (e) => {
+    this.setState({
+      text: "You clicked!",
+    });
+  };
+  render() {
+    return (
+      <div>
+        <button type="button" onClick={this.changeState}>
+          {this.state.text}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-function clock() {
-  const element = (
-    <div>
-      <h2>Time now is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById("root"));
-}
 
-setInterval(clock, 1000);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
