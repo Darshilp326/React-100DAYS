f480e7763cad03fc09dc64a89e776894d781df7c
Darshilp326 <darshil_1901me18@iitp.ac.in>
2020-09-22 14:36:54 +0530

    Day5

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+//import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
