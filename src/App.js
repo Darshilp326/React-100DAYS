d93a721af21731068ed7f20f0c0f443adbd8188e
Darshilp326 <darshil_1901me18@iitp.ac.in>
2020-09-23 14:34:21 +0530

    Day6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from "react";
 import logo from "./logo.svg";
+import Car from "./Car";
 import "./App.css";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: "Click here!" };
   }
-  changeState = (e) => {
-    this.setState({
-      text: "You clicked!",
-    });
-  };
   render() {
     return (
       <div>
-        <button type="button" onClick={this.changeState}>
-          {this.state.text}
-        </button>
+        <Car />
       </div>
     );
   }
 }
 
-//export default App;
+export default App;
