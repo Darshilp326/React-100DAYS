d2141d95a39fc201fe9dc394a6c33aab9ca5cf2d
Darshilp326 <darshil_1901me18@iitp.ac.in>
2020-09-27 22:54:21 +0530

    Day 10

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import Car from "./Car";
 import Table from "./Table";
 import User from "./User";
-import "./App.css";
+//import "./App.css";
 class App extends React.Component {
   constructor(props) {
     super(props);
