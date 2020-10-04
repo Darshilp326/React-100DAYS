cc9c30d62c01283c682ccb3ca99c17ade33e9891
Darshilp326 <darshil_1901me18@iitp.ac.in>
2020-10-04 19:51:43 +0530

    Day 17

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import RenderList from "./RenderList";
+import GetSnapshotBeforeUpdate from "./GetSnapshotBeforeUpdate";
 class App extends React.Component {
   render() {
     return (
       <div>
-        <RenderList />
+        <GetSnapshotBeforeUpdate />
       </div>
     );
   }
