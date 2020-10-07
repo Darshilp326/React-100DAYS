ad2bd13ef49cd39b7c8e508f1e63ca75bb88b9ef
Darshilp326 <darshil_1901me18@iitp.ac.in>
2020-10-07 12:34:07 +0530

    Day 20

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import ComponentWillUnmount from "./ComponentWillUnmount";
+import List from "./List";
 class App extends React.Component {
   render() {
     return (
       <div>
-        <ComponentWillUnmount />
+        <List />
       </div>
     );
   }
