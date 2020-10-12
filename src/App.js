facceadb4eed7c5eb18360909a72da4ddb9bc86c
Darshilp326 <darshil_1901me18@iitp.ac.in>
2020-10-12 19:24:43 +0530

    Day 24

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Form from "./Form";
+import FormValidation from "./FormValidation";
 class App extends React.Component {
   render() {
     return (
       <div>
-        < Form/>
+        < FormValidation/>
       </div>
     );
   }
